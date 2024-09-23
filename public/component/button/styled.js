e8d381f4e2b34e5cf23d13d0602bfacfa69b5b02
Newton <ephraimnewton533@gmail.com>
2024-09-23 12:48:50 +0100

    youngest landlord

---
+++ public/component/button/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button(({ width, backgroundcolor }) => {
+  return {
+    //styles for button should begin below this line.
+    textAlign: "center",
+    backgroundColor: backgroundcolor || "#4B95FF",
+    border: "none",
+    color: "white",
+    borderRadius: "8px",
+    width: width || "-webkit-fill-available",
+    padding: "calc(var(--cardPadding)/2) var(--cardPadding)",
+    fontSize: "1rem",
+    cursor: "pointer",
+    "@media screen and (max-width: 425px)": {
+      width: "-webkit-fill-available",
+    },
+  };
+});
