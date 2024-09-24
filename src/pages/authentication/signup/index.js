c161d720e65efb373e1414eed503cbdd0d645f96
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-09-24 15:43:59 +0100

    I have now cleaned up code here, including entire typography styles

--- src/pages/authentication/signup/index.js
+++ src/pages/authentication/signup/index.js
@@ -1,10 +1,17 @@
 import { SignUpWrapper } from "./styled";
-import { BaseButton } from "../../../component/button/styled";
+import { BaseButton } from "../../../components/button/styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export const SignUp = () => {
   return (
     <SignUpWrapper>
-      <BaseButton>Get Started</BaseButton>
+      <BaseButton
+        width={"fit-content"}
+      >
+        Get Started
+      </BaseButton>
+      <FontAwesomeIcon icon={faHouse} />
     </SignUpWrapper>
-  );
+  )
 };
