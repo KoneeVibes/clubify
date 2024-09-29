4bad93c64bb6f3c70ea649d9f552ea20a5154e50
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-09-29 22:55:57 +0100

    unblocking session with Ibk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SignUp } from './pages/authentication/signup';
+import { SignIn } from './pages/authentication/signin';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/authentication/signup/index.js
+++ src/pages/authentication/signup/index.js
@@ -1,5 +1,5 @@
 import { SignUpWrapper, SignUpRow } from "./styled";
-import { H3, P, Label,A } from "../../../components/typography/styled";
+import { H3, P, Label, A } from "../../../components/typography/styled";
 import { BaseFieldSet } from "../../../components/form/fieldset/styled";
 import { BaseInput } from "../../../components/form/input/styled"
 import { BaseButton } from "../../../components/button/styled";
@@ -10,8 +10,31 @@ import { Hrworkplace } from "../../../assets";
 import { SignUpimg } from "../../../assets";
 import { Column } from "../../../components/flex/styled";
 import { Google } from "../../../assets";
+import { Fragment, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
+    // update below with form controller
+    const navigate = useNavigate();
+    const [formDetails, setFormDetails] = useState();
+    const [step, setStep] = useState(1);
+
+    const handleClickNext = (e, step) => {
+        if (step === 2) {
+            // submitting signup form.
+            return navigate("/signin");
+        };
+        setStep((prev) => {
+            return prev + 1;
+        });
+    };
+
+    const handleClickPrevious = () => {
+        setStep((prev) => {
+            return prev - 1;
+        })
+    };
+
     return (
         <SignUpWrapper tocolumn={true}>
             <Column className="reg-form">
@@ -24,81 +47,103 @@ export const SignUp = () => {
                 </div>
 
                 <form>
-                    <SignUpRow>
-                        <BaseFieldSet>
-                            <Label>Firstname</Label>
-                            <BaseInput 
-                                type="text"
-                                name="firstname"
-                                // style= {{
-                                //     width: "40%",
-                                // }}
-                                // value={referForm.firstName}
-                                // onChange={(e) => handleChange(e)}
-                                required
-                            />
-                        </BaseFieldSet>
-                        <BaseFieldSet>
-                            <Label>Lastname</Label>
+                    {step === 1 && (
+                        <Fragment>
+                            <SignUpRow>
+                                <BaseFieldSet>
+                                    <Label>Firstname</Label>
+                                    <BaseInput
+                                        type="text"
+                                        name="firstname"
+                                        // style= {{
+                                        //     width: "40%",
+                                        // }}
+                                        // value={referForm.firstName}
+                                        // onChange={(e) => handleChange(e)}
+                                        required
+                                    />
+                                </BaseFieldSet>
+                                <BaseFieldSet>
+                                    <Label>Lastname</Label>
+                                    <BaseInput
+                                        type="text"
+                                        name="lastname"
+                                        // style= {{
+                                        //     width: "40%",
+                                        // }}
+                                        // value={referForm.lastName}
+                                        // onChange={(e) => handleChange(e)}
+                                        required
+                                    />
+                                </BaseFieldSet>
+                            </SignUpRow>
+                            <Label>Email</Label>
                             <BaseInput
-                                type="text"
-                                name="lastname"
-                                // style= {{
-                                //     width: "40%",
-                                // }}
-                                // value={referForm.lastName}
+                                type="email"
+                                name="email"
+                                placeholder="Enter email here"
+                                style={{
+                                    width: "-webkit-fill-available",
+                                }}
+                                // value={referForm.companyEmail}
                                 // onChange={(e) => handleChange(e)}
                                 required
                             />
-                        </BaseFieldSet>
-                    </SignUpRow>
-                    <Label>Email</Label>
-                    <BaseInput
-                        type="email"
-                        name="email"
-                        placeholder="Enter email here"
-                        style= {{
-                            width: "-webkit-fill-available",
-                        }}
-                        // value={referForm.companyEmail}
-                        // onChange={(e) => handleChange(e)}
-                        required
-                    />
-                    <Label>Password</Label>
-                    <BaseInput
-                        type="password"
-                        name=""
-                        placeholder=""
-                        style= {{
-                            width: "-webkit-fill-available",
-                        }}
-                        // value={referForm.companyEmail}
-                        // onChange={(e) => handleChange(e)}
-                        required
-                    />
-                    <Label>Repeat Password</Label>
-                    <BaseInput
-                        type="password"
-                        name=""
-                        placeholder=""
-                        style= {{
-                            width: "-webkit-fill-available",
-                        }}
-                        // value={referForm.companyEmail}
-                        // onChange={(e) => handleChange(e)}
-                        required
-                    />
-                    <BaseButton>Next</BaseButton>
+                            <Label>Password</Label>
+                            <BaseInput
+                                type="password"
+                                name=""
+                                placeholder=""
+                                style={{
+                                    width: "-webkit-fill-available",
+                                }}
+                                // value={referForm.companyEmail}
+                                // onChange={(e) => handleChange(e)}
+                                required
+                            />
+                            <Label>Repeat Password</Label>
+                            <BaseInput
+                                type="password"
+                                name=""
+                                placeholder=""
+                                style={{
+                                    width: "-webkit-fill-available",
+                                }}
+                                // value={referForm.companyEmail}
+                                // onChange={(e) => handleChange(e)}
+                                required
+                            />
+                            <BaseButton
+                                onClick={(e) => handleClickNext(e, step)}
+                            >
+                                Next
+                            </BaseButton>
+                        </Fragment>
+                    )}
+                    {step === 2 && (
+                        <Fragment>
+                            {/* Form for step 2 including the previous and next buttons */}
+                            <BaseButton
+                                onClick={(e) => handleClickPrevious(e, step)}
+                            >
+                                Previous
+                            </BaseButton>
+                            <BaseButton
+                                onClick={(e) => handleClickNext(e, step)}
+                            >
+                                Sign up
+                            </BaseButton>
+                        </Fragment>
+                    )}
                     <BaseButton backgroundcolor={"#ffffff"} className="google-btn">
-                       <Google/>Sign Up with Google
+                        <Google />Sign Up with Google
                     </BaseButton>
                     <div className="sign-in-link">
-                      <P>Already have an account? <A href="/sign-in">Sign In</A></P> 
-                      <Hrworkplace/>
+                        <P>Already have an account? <A href="/sign-in">Sign In</A></P>
+                        <Hrworkplace />
                     </div>
-                    
                 </form>
-                
+
             </Column>
             <div className="reg-image">
                 <SignUpimg />
