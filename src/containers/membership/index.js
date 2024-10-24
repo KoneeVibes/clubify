5a2ddc97f38fec7ecc176083555d10c0ecc3cdba
Abdulgafar <abdalaka20045@gmail.com>
2024-10-24 13:58:40 +0100

    i passed in layout props for all members page and developed

--- src/containers/membership/index.js
+++ src/containers/membership/index.js
@@ -1,25 +1,26 @@
 import { MembershipWrapper } from "./styled";
 import { Layout } from "../layout/index";
 import Cookies from "universal-cookie";
+import { AllMembersWrapper } from "../allmembers/styled";
 
 export const Membership = () => {
     const cookies = new Cookies();
     const profileDetails = cookies.get("profile");
     return (
         <Layout
-            title={`Hello ${profileDetails?.member?.firstname || ""}`}
+            title={`Hello ${profileDetails?.allmembers?.firstname || ""}`}
             subTitle={new Date().toLocaleDateString('en-US', {
                 day: 'numeric',
                 month: 'long',
                 weekday: 'long'
             })}
             plan={profileDetails?.plan?.planName}
-            fullName={`${profileDetails?.member?.firstname || ""} ${profileDetails?.member?.lastname || ""}`}
+            fullName={`${profileDetails?.allmembers?.firstname || ""} ${profileDetails?.allmembers?.lastname || ""}`}
         >
-            <MembershipWrapper>
+            <AllMembersWrapper>
                 {/* Newton, your html goes in here */}
 
-            </MembershipWrapper>
+            </AllMembersWrapper>
         </Layout>
     )
 }
