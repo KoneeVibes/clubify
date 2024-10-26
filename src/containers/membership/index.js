8b35e8391349611500bef3198ca8131dba2cb978
Newton <ephraimnewton533@gmail.com>
2024-10-27 00:04:44 +0100

    i am having issue figuring out that input part

--- src/containers/membership/index.js
+++ src/containers/membership/index.js
@@ -1,25 +1,108 @@
 import { MembershipWrapper } from "./styled";
 import { Layout } from "../layout/index";
 import Cookies from "universal-cookie";
+import { Column, BaseFlex, Row } from "../../components/flex/styled";
+import { H2, H3, P, Span } from "../../components/typography/styled";
+import { FaUpload } from "react-icons/fa";
+import { CardWrapper } from "../../components/card/styled";
+// import { BaseInput } from "../../components/form/input/styled";
 
 export const Membership = () => {
-    const cookies = new Cookies();
-    const profileDetails = cookies.get("profile");
-    return (
-        <Layout
-            title={`Hello ${profileDetails?.member?.firstname || ""}`}
-            subTitle={new Date().toLocaleDateString('en-US', {
-                day: 'numeric',
-                month: 'long',
-                weekday: 'long'
-            })}
-            plan={profileDetails?.plan?.planName}
-            fullName={`${profileDetails?.member?.firstname || ""} ${profileDetails?.member?.lastname || ""}`}
-        >
-            <MembershipWrapper>
-                {/* Newton, your html goes in here */}
+  const cookies = new Cookies();
+  const profileDetails = cookies.get("profile");
+  return (
+    <Layout
+      title={`Hello ${profileDetails?.member?.firstname || ""}`}
+      subTitle={new Date().toLocaleDateString("en-US", {
+        day: "numeric",
+        month: "long",
+        weekday: "long",
+      })}
+      plan={profileDetails?.plan?.planName}
+      fullName={`${profileDetails?.member?.firstname || ""} ${
+        profileDetails?.member?.lastname || ""
+      }`}
+    >
+      <MembershipWrapper>
+        {/* Newton, your html goes in here */}
+        <div className="membershipPlanContainer">
+          <div className="membershipDetails">
+            <H2>Membership Plan</H2>
+            <CardWrapper className="fieldRow">
+              <Row className="rowDirection">
+                <Span>Plan Name</Span>
+                <Span className="highlight">Gold</Span>
+              </Row>
+              <Row className="rowDirection color">
+                <Span>Status</Span>
+                <Span>Verified</Span>
+              </Row>
+              <Row className="rowDirection">
+                <Span>Start Date</Span>
+                <Span>02, August 2022</Span>
+              </Row>
+              <Row className="rowDirection color">
+                <Span>End Date</Span>
+                <Span>02, August 2023</Span>
+              </Row>
+              <Row className="rowDirection">
+                <Span>Auto-renew</Span>
+                <Span>False</Span>
+              </Row>
+            </CardWrapper>
+          </div>
 
-            </MembershipWrapper>
-        </Layout>
-    )
-}
+          <div className="membership-options">
+            <H3>Membership Options & History</H3>
+            <CardWrapper className="membershipOptions">
+              <BaseFlex className="option" justifycontent={"space-between"}>
+                <Column
+                  style={{
+                    gap: "0",
+                  }}
+                >
+                  <Span>Upload Membership Documents</Span>
+                  <P>Keep Your Membership Information Updated</P>
+                </Column>
+                {/* <InputRow> */}
+                {/* <BaseInput /> */}
+                <FaUpload className="icon" />
+                {/* </InputRow> */}
+              </BaseFlex>
+              <hr></hr>
+              <div className="option">
+                <Span>Upgrade Membership Plan</Span>
+                <P>Explore Membership upgrades</P>
+              </div>
+              <hr></hr>
+              <div className="option">
+                <Span>View Membership History</Span>
+                <P>Your Membership Timeline</P>
+              </div>
+              <hr></hr>
+              <div className="option">
+                <Span>Setup Auto-renewal</Span>
+                <p>Seamless Renewal; Never Miss a Beat</p>
+              </div>
+              <hr></hr>
+              <div className="option">
+                <Span>Membership FAQs</Span>
+                <p>Check out our Frequently Asked Questions</p>
+              </div>
+              <hr></hr>
+              <div className="option contact-support">
+                <Span>Contact Support</Span>
+                <P>Get in Touch Today</P>
+              </div>
+              <hr></hr>
+              <div className="option cancel-membership">
+                <Span>Cancel Membership</Span>
+                <P>End Membership Subscription</P>
+              </div>
+            </CardWrapper>
+          </div>
+        </div>
+      </MembershipWrapper>
+    </Layout>
+  );
+};
--- src/containers/membership/styled.js
+++ src/containers/membership/styled.js
@@ -1,7 +1,39 @@
 import styled from "styled-components";
+import { Row } from "../../components/flex/styled";
 
 export const MembershipWrapper = styled("div")(() => {
-    return {
-        // Newton, your CSS goes in here 
-    }
-})
+  return {
+    backgroundColor: "#FFFFFF",
+    gap: "0",
+    "& H2": {
+      marginBlock: 0,
+      fontSize: "24px",
+    },
+    "& H3": {
+      fontSize: "20px",
+      fontWeight: "700",
+    },
+    "& .fieldRow": {
+      backgroundColor: "#D9D9D9",
+      padding: "calc(var(--basic-padding)/3) ",
+    },
+    "& .hr": {
+      border: "1px solid #3C1985",
+    },
+    "& .color": {
+      backgroundColor: "#EFEFEF",
+    },
+    "& .membershipOptions": {
+      backgroundColor: "#EFEFEF",
+    },
+    "& .option": {
+      gap: "calc(var(--flex-gap)/2)",
+    },
+    "& .rowDirection": {
+      justifyContent: "space-between",
+    },
+    "& .highlight": {
+      color: "#FFC00E",
+    },
+  };
+});
