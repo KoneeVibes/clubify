d8c08319cf4c24f36c9352a69ab9e513bc22bb64
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-10-28 10:20:33 +0100

    completed static UI development for all members page

--- src/containers/allmembers/styled.js
+++ src/containers/allmembers/styled.js
@@ -3,8 +3,10 @@ import styled from "styled-components";
 export const AllMembersWrapper = styled("div")(() => {
     return {
         display: "grid",
-        gridTemplateColumns: "repeat(3, minmax(150px, 1fr))",
+        gridTemplateColumns: "repeat(1, minmax(150px, 1fr))",
         gap: "var(--flex-gap)",
+        marginBlockEnd: "calc(var(--basic-margin) * 1.5)",
+        overflow: "hidden",
         "& .card": {
             padding: 0,
             "& h3": {
@@ -30,6 +32,12 @@ export const AllMembersWrapper = styled("div")(() => {
                 fontSize: "15px",
                 marginLeft: "0.2rem",
             }
+        },
+        "@media screen and (min-width: 425px)": {
+            gridTemplateColumns: "repeat(2, minmax(150px, 1fr))",
+        },
+        "@media screen and (min-width: 1440px)": {
+            gridTemplateColumns: "repeat(3, minmax(150px, 1fr))",
         }
     }
 })
