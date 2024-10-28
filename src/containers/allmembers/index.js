e6010b06232380f839ad286f9d00496532c2f03d
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-10-28 07:57:27 +0100

    begun work on all members

--- src/containers/allmembers/index.js
+++ src/containers/allmembers/index.js
@@ -7,14 +7,23 @@ import { ProfileIcon } from "../../assets";
 export const AllMembers = () => {
     const cookies = new Cookies();
     const profileDetails = cookies.get("profile");
-
+    
     return (
-        <Layout>
+        <Layout
+            title={`Hello ${profileDetails?.member?.firstname || ""}`}
+            subTitle={new Date().toLocaleDateString('en-US', {
+                day: 'numeric',
+                month: 'long',
+                weekday: 'long'
+            })}
+            plan={profileDetails?.plan?.planName}
+            fullName={`${profileDetails?.member?.firstname || ""} ${profileDetails?.member?.lastname || ""}`}
+        >
             <AllMembersWrapper>
                 <Card>
                     {/* your code goes in here */}
                     <ProfileIcon />
-                    
+
                 </Card>
             </AllMembersWrapper>
         </Layout>
