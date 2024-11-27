f3bba3abe79ca1b435e4f7aea9cf9a468d6b85a6
Ibukun <ibukunoladiporaji@gmail.com>
2024-11-27 16:21:04 +0100

    Successful on Event

--- src/pages/app/events/index.js
+++ src/pages/app/events/index.js
@@ -1,10 +1,40 @@
 import { EventsWrapper } from "./styled";
 import { Layout } from "../../../containers/layout/index";
+import { Filter } from "../../../assets";
+import { BaseFieldSet } from "../../../components/form/fieldset/styled";
+import { BaseSelect } from "../../../components/form/select/styled";
+import { Row } from "../../../components/flex/styled";
+import { Span, H1 } from "../../../components/typography/styled";
+import { getAllEvent } from "../../../utils/apis/getAllEvent";
+import { useEffect, useState } from "react";
+import { Business, Game, Easter, Party, PoolParty, Workshop } from "../../../assets";
 import Cookies from "universal-cookie";
 
 export const Events = () => {
+    const [filter, setFilter] = useState({
+        months: "",
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFilter((prev) => ({
+          ...prev,
+          [name]: value,
+        }));
+      };
     const cookies = new Cookies();
-    const { profile } = cookies.getAll();
+    const { profile, data } = cookies.getAll();
+    const [profileDetails, setProfileDetails] = useState({});
+
+    useEffect(() => {
+        getAllEvent(data.token)
+            .then((detail) => {
+                setProfileDetails(detail);
+            })
+            .catch((err) => {
+                console.error("Failed to fetch projects:", err);
+            })
+    }, [data]);
 
     return (
         <Layout
@@ -19,7 +49,41 @@ export const Events = () => {
             fullName={profile?.role === "administrator" ? `${profile?.firstname || ""} ${profile?.lastname || ""}` : `${profile?.member?.firstname || ""} ${profile?.member?.lastname || ""}`}
         >
             <EventsWrapper>
-
+                <Row
+                    className="header"
+                    alignitems="center"
+                    justifycontent="space-between"
+                >
+                    <div>
+                        <H1>Events</H1>
+                    </div>
+                    <Row
+                        className="filter"
+                        alignitems="center"
+                    >
+                        <Filter />
+                        <Span>Filter</Span>
+                        <BaseFieldSet>
+                            <BaseSelect
+                                name="months"
+                                onChange={handleChange}
+                                value={filter.months}
+                            >
+                                <option value="">This Months</option>
+                                <option value="1">April</option>
+                                <option value="2">May</option>
+                            </BaseSelect>
+                        </BaseFieldSet>
+                    </Row>
+                </Row>
+                <Row>
+                    <Business/>
+                    <Game/>
+                    <Easter/>
+                    <Party/>
+                    <PoolParty/>
+                    <Workshop/>
+                </Row>
             </EventsWrapper>
         </Layout>
     )
