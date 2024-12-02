0d9f3213bb9be3a13995114516562d20cb769e41
Newton <ephraimnewton533@gmail.com>
2024-12-02 10:06:23 +0100

    event

--- src/pages/app/events/eventdetails/index.js
+++ src/pages/app/events/eventdetails/index.js
@@ -2,7 +2,8 @@ import { EventDetailsWrapper } from "./styled";
 import { Layout } from "../../../../containers/layout/index";
 import Cookies from "universal-cookie";
 import eventImage from "../../../../assets/images/eventimg.jpg";
-import { H1, H2, H3, Span } from "../../../../components/typography/styled";
+import { H1, H2, H3, P, Span } from "../../../../components/typography/styled";
+import { Row, Column } from "../../../../components/flex/styled";
 
 export const EventDetails = () => {
   const cookies = new Cookies();
@@ -39,17 +40,19 @@ export const EventDetails = () => {
           <img src={eventImage} alt="diplomatic agreement" />
         </div>
         <Column className="hero-text">
-          <H2>why workPlacePay?</H2>
-          <H3>Empower your business with seamless payroll management</H3>
-          <P>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in
-            sem orci. Nunc a sagittis lacus. Nunc turpis augue, sodales quis
-            fermentum vitae, vestibulum a orci. Donec vitae ultrices odio.
-            Mauris condimentum id arcu sit amet venenatis. Quisque vitae nisi
-            egestas, varius lorem eget, aliquam neque. Donec ornare egestas
-            lacinia. Nulla pellentesque eleifend ullamcorper.
-          </P>
-          <BaseButton width={"fit-content"}>Get Started</BaseButton>
+          <Span>Event Name</Span>
+          <P>Annual Business Conference</P>
+          <P>2022</P>
+          <Span>Start Date</Span>
+          <P>20, March 2022 - 10:00 AM</P>
+          <br></br>
+          <Span>End Date</Span>
+          <P>20, March 2022 - 03:00 PM</P>
+          <br></br>
+          <Span>Location</Span>
+          <P>Conference Room 302</P>
+          <br></br>
+          <Span>Description</Span>
         </Column>
       </EventDetailsWrapper>
     </Layout>
--- src/pages/app/events/eventdetails/styled.js
+++ src/pages/app/events/eventdetails/styled.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 
 export const EventDetailsWrapper = styled("div")(() => {
-    return {
-
-    }
-})
+  return {
+    margin: "calc(var(--sectionMargin) * 2) 0",
+    padding: "0 var(--pagePadding)",
+    gap: "calc(var(--flexGap) * 2)",
+    ".hero-img, .hero-text": {
+      flex: 1,
+    },
+    ".hero-img>img": {
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+      borderRadius: "1rem",
+    },
+    ".hero-text>h2, .hero-text>h3, .hero-text>p ": {
+      marginBlock: 0,
+    },
+    ".hero-text": {
+      gap: "calc(var(--flexGap)/2)",
+      overflow: "hidden",
+    },
+    "@media screen and (min-width: 1024px)": {
+      margin: "calc(var(--sectionMargin) * 3) 0",
+    },
+  };
+});
