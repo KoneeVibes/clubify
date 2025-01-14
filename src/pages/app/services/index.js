1face1ea2f70e090ef6e76f17e9733caa2c10cc1
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-01-14 18:06:35 +0100

    I created the filter row in services page

--- src/pages/app/services/index.js
+++ src/pages/app/services/index.js
@@ -1,6 +1,11 @@
 import { ServicesWrapper } from "./styled";
 import { Layout } from "../../../containers/layout/index";
 import Cookies from "universal-cookie";
+import { Row } from "../../../components/flex/styled";
+import { H1, Span } from "../../../components/typography/styled";
+import { Filter } from "../../../assets";
+import { BaseFieldSet } from "../../../components/form/fieldset/styled";
+import { BaseSelect } from "../../../components/form/select/styled";
 
 export const Services = () => {
     const cookies = new Cookies();
@@ -8,7 +13,7 @@ export const Services = () => {
 
     return (
         <Layout
-        role={profile?.role}
+            role={profile?.role}
             title={`Hello ${profile.role === "administrator" ? profile?.firstname || "" : profile?.member?.firstname || ""}`}
             subTitle={new Date().toLocaleDateString('en-US', {
                 day: 'numeric',
@@ -19,7 +24,28 @@ export const Services = () => {
             fullName={profile?.role === "administrator" ? `${profile?.firstname || ""} ${profile?.lastname || ""}` : `${profile?.member?.firstname || ""} ${profile?.member?.lastname || ""}`}
         >
             <ServicesWrapper>
-
+                <Row
+                    className="header"
+                    alignitems="center"
+                    justifycontent="space-between"
+                >
+                    <div>
+                        <H1>Services</H1>
+                    </div>
+                    <Row
+                        className="filter"
+                        alignitems="center"
+                    >
+                        <Filter />
+                        <Span>Filter</Span>
+                        <BaseFieldSet>
+                            <BaseSelect name="service">
+                                <option value="">Service Type</option>
+                                {/* Add additional location options here */}
+                            </BaseSelect>
+                        </BaseFieldSet>
+                    </Row>
+                </Row>
             </ServicesWrapper>
         </Layout>
     )
--- src/pages/app/services/styled.js
+++ src/pages/app/services/styled.js
@@ -2,6 +2,56 @@ import styled from "styled-components";
 
 export const ServicesWrapper = styled("div")(() => {
     return {
+        overflow: "hidden",
+        marginBottom: "var(--basic-margin)",
+        "& .header": {
+            marginBottom: "20px",
+            "& h1": {
+                fontSize: "24px",
+                fontWeight: "bold",
+                color: "#333",
+            },
+            "& fieldset": {
+                overflow: "hidden",
+                display: "flex",
+                flexDirection: "column",
+            },
+            "& select": {
+                padding: "calc(var(--basic-padding)/5)",
+                borderRadius: "10px",
+            },
+            "& div:first-of-type": {
+                width: "100%"
+            },
+            "@media screen and (max-width: 565px)": {
+                gap: "calc(var(--flex-gap)/3)",
+                alignItems: "flex-start",
+                flexDirection: "column",
+            },
+            "@media screen and (min-width: 768px) and (max-width: 890px)": {
+                gap: "calc(var(--flex-gap)/3)",
+                alignItems: "flex-start",
+                flexDirection: "column",
+            },
+            "@media screen and (min-width: 1024px) and (max-width: 1156px)": {
+                gap: "calc(var(--flex-gap)/3)",
+                alignItems: "flex-start",
+                flexDirection: "column",
+            },
+        },
 
+        "& .header .filter": {
+            gap: "8px",
+            "& span": {
+                fontSize: "16px",
+                color: "#555",
+                fontWeight: "500",
+            },
+            "@media screen and (max-width: 365px)": {
+                gap: "calc(var(--flex-gap)/3)",
+                alignItems: "flex-start",
+                flexDirection: "column",
+            },
+        },
     }
 })
