e70346d8e521eb3736bf18df4651bbbf939d6924
Konee <koneevibes@gmail.com>
2025-04-09 14:15:45 +0100

    reviewed and gave feedback

--- src/pages/app/profile/index.js
+++ src/pages/app/profile/index.js
@@ -1,19 +1,17 @@
 import Cookies from "universal-cookie";
 import { Layout } from "../../../containers/layout";
 import { ProfileWrapper } from "./styled";
-import { H2, P } from "../../../components/typography/styled";
+import { H2, P, Span } from "../../../components/typography/styled";
 import { Card } from "../../../components/card";
 import { BaseButton } from "../../../components/button/styled";
 import picture from "../../../assets/images/User icon.svg";
 import pic from "../../../assets/gold badge.svg";
-import { Row } from "../../../components/flex/styled";
-
-
+import { Column, Row } from "../../../components/flex/styled";
 
 export const Profile = () => {
   const cookies = new Cookies();
   const { profile } = cookies.getAll();
-    
+
   return (
     <Layout
       role={profile?.role}
@@ -27,26 +25,35 @@ export const Profile = () => {
       fullName={profile?.role === "administrator" ? `${profile?.firstname || ""} ${profile?.lastname || ""}` : `${profile?.member?.firstname || ""} ${profile?.member?.lastname || ""}`}
     >
       <ProfileWrapper>
-      <div className="background"/>  
-      <Card>
-          <img src={picture} alt= "user icon"/>
-           <H2>Sarah Adebowale</H2> 
-           <Row className="badge">
-            <img src={pic} alt="gold badge"/>
-            <P>Gold Plan Staff</P>
-           </Row>
-           <P>+234 8083876042</P>
-           <P>DOB: 17/04/2005</P>
-           <P>Female</P>
-         <BaseButton
-            className="change-button"
-          >
-            <span>
-              Edit Profile
-           </span>
-          </BaseButton>
-      </Card>
-     </ProfileWrapper>
+        <div className="background" />
+        <Column>
+          <div style={{ height: "113px" }} />
+          <Card>
+            <img src={picture} alt="user icon" className="user-icon" />
+            <H2>Sarah Adebowale</H2>
+            <Row className="badge">
+              <img src={pic} alt="gold badge" />
+              <P>
+                <Span className="gold">Gold</Span>
+                {/* display: inline-block */}
+                Plan
+                Staff
+              </P>
+            </Row>
+            <P>+234 8083876042</P>
+            <P>DOB: 17/04/2005</P>
+            <P>Female</P>
+            <BaseButton
+              className="change-button"
+            >
+              <span>
+                Edit Profile
+              </span>
+            </BaseButton>
+          </Card>
+          <div />
+        </Column>
+      </ProfileWrapper>
     </Layout>
   );
 };
--- src/pages/app/profile/styled.js
+++ src/pages/app/profile/styled.js
@@ -3,61 +3,68 @@ import wavy from "../../../assets/twistline3.svg";
 import curly from "../../../assets/bluetwistline.svg";
 
 export const ProfileWrapper = styled("div")(() => {
-    return {
-        "& .background": {
-          backgroundColor: "#3C1985",
-          width: "100%",
-          height: "70px",
-          backgroundImage:`url(${wavy})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          marginBottom: "40px",
-        },
-        "& .card": {
-          backgroundImage:`url(${curly})`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          boxShadow:  "10px 10px 20px rgba(0, 0, 0, 0.5)",
-          height: "400px",
-          marginLeft: "auto",
-          marginRight: "auto"
-        },
-        "& p": {
-          fontSize: "18px",
-          fontWeight: "600",
-          textAlign: "center",
-        },
-        "& h2": {
-          fontSize: "36px",
-          fontWeight: "400",
-          lineHeight: "100%",
-          textAlign: "center",
-        },
-        "& .change-button": {
-          borderRadius: "10px",
-          overflow: "hidden",
-          height: "14px",
-          width: "150px",
-          backgroundColor: "#3C1985",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          "& p": {
-          fontSize: "16px",
-          fontWeight: 600,
-          color: "#EFEFEF",
-          marginBlock: 0,
-        },
-        },
-        "& img": {
-          borderRadius: "30px",
-          justifyContent: "flex-start",
-          display: "flex",
-        },
-        "& .badge": {
-          justifyContent: "center",
-          gap: "calc(var(--flex-gap)/10)",
-        }
-      }
+  return {
+    "& .background": {
+      backgroundColor: "#3C1985",
+      width: "100%",
+      height: "113px",
+      backgroundImage: `url(${wavy})`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      position: "absolute",
+      left: "0",
+      right: "0",
+    },
+    "& .card": {
+      backgroundImage: `url(${curly})`,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      // boxShadow: "10px 10px 20px rgba(0, 0, 0, 0.5)",
+      marginLeft: "auto",
+      marginRight: "auto",
+      position: "relative",
+      overflow: "visible",
+    },
+    "& p": {
+      fontSize: "18px",
+      fontWeight: "600",
+      textAlign: "center",
+    },
+    "& h2": {
+      fontSize: "36px",
+      fontWeight: "400",
+      lineHeight: "100%",
+      textAlign: "center",
+    },
+    "& .change-button": {
+      borderRadius: "10px",
+      overflow: "hidden",
+      height: "14px",
+      width: "150px",
+      backgroundColor: "#3C1985",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      "& p": {
+        fontSize: "16px",
+        fontWeight: 600,
+        color: "#EFEFEF",
+        marginBlock: 0,
+      },
+    },
+    "& .user-icon": {
+      borderRadius: "30px",
+      justifyContent: "flex-start",
+      display: "flex",
+      position: "absolute",
+      left: "-25px",
+      top: "-55px"
+    },
+    "& .badge": {
+      justifyContent: "center",
+      gap: "calc(var(--flex-gap)/10)",
+    }
+  }
 })
